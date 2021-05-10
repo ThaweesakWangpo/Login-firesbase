1c79ba81d153f7dbd25073696af601f01cb1ae3d
ThaweesakWangpo <69568163+ThaweesakWangpo@users.noreply.github.com>
2021-05-10 16:22:14 +0700

    login-firebase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
+
+import Login from './components/Login'; 
+import auth from './firebase';
+
 import './App.css';
 
+
 function App() {
+  const [session, setSession] = useState({
+    isLoggedIn: false,
+    currentUser: null,
+    errorMessage: null
+  });
+
+  useEffect(() => {
+    const handleAuth = auth.onAuthStateChanged(user => {
+      if (user) {
+        setSession({
+          isLoggedIn: true,
+          currentUser: user,
+          errorMessage: null
+        });
+      }
+    });
+
+    return () => {
+      handleAuth();
+    };
+  }, []);
+
+  const handleLogout = () => {
+    auth.signOut().then(response => {
+      setSession({
+        isLoggedIn: false,
+        currentUser: null
+      });
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
+      {
+        session.isLoggedIn ? (
+        <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +56,15 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+        <h1>Ahoy!, {session.currentUser && session.currentUser.email}</h1>
+
+        <button type="button" onClick={handleLogout}>
+            Logout
+          </button>
+
+      </header>) : (<Login setSession = {setSession} />)
+      }
+ 
     </div>
   );
 }
---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAT3LNh5PfgUrVscVUiwd0ULwXbUMsIZq4",
+    authDomain: "tarotdb-firebase.firebaseapp.com",
+    projectId: "tarotdb-firebase",
+    storageBucket: "tarotdb-firebase.appspot.com",
+    messagingSenderId: "166771798831",
+    appId: "1:166771798831:web:3ec3e4fd191d4e9682e7da",
+    measurementId: "G-6SH3T2DGP4"
+  };
+
+  export default firebaseConfig;
